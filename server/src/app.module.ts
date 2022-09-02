f5934264c3f77f0cfc7f1f07f7ea075959c13159
Georg Schwarz <dev@georg-schwarz.com>
2022-09-02 15:01:25 +0200

    Add config files to server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.dev.local'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
