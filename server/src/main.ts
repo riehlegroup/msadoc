0eb1f7241ad6253062a2619bc7ee6206339dbca5
Georg Schwarz <dev@georg-schwarz.com>
2022-09-02 14:44:39 +0200

    Add openapi to server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('msdoc server API')
+    .setDescription('The msdoc server API description')
+    .setVersion('1.0')
+    .addTag('server')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
