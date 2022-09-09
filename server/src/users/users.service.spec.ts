6a97a6269a26acfbe958d7ea6164bab6540ab690
MBuchalik <dev@martin-buchalik.de>
2022-09-09 18:58:31 +0200

    Fix wrong env variable name in tests

--- server/src/users/users.service.spec.ts
+++ server/src/users/users.service.spec.ts
@@ -13,9 +13,9 @@ describe('UsersService', () => {
           provide: ConfigService,
           useValue: {
             get: jest.fn((key: string) => {
-              if (key === 'username') {
+              if (key === 'user_name') {
                 return 'admin';
-              } else if (key === 'password') {
+              } else if (key === 'user_password') {
                 return 'password';
               }
               return null;
