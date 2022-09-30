d5ba1cd38f73635ccf5f042e90c3cb7846573129
MBuchalik <dev@martin-buchalik.de>
2022-09-30 15:22:54 +0200

    Show basic information about the currently selected service (#41)

--- frontend/src/components/main-page/groups-tree-page/service-details/service-details.tsx
+++ frontend/src/components/main-page/groups-tree-page/service-details/service-details.tsx
@@ -1,3 +1,12 @@
+import { Badge, Code, Group, TaskAlt } from '@mui/icons-material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
 import { GetServiceDocResponse } from 'msadoc-client';
 import React from 'react';
 import { useMatch } from 'react-router-dom';
@@ -10,7 +19,66 @@ export const ServiceDetails: React.FC = () => {
 
   return (
     <React.Fragment>
-      Service Details {JSON.stringify(controller.service)}
+      {controller.service && (
+        <Box
+          sx={{
+            overflowX: 'hidden',
+            overflowY: 'auto',
+            padding: 4,
+            maxWidth: '700px',
+          }}
+        >
+          <Typography variant="h3">Base Information</Typography>
+
+          <List>
+            <ListItem divider>
+              <ListItemIcon>
+                <Badge />
+              </ListItemIcon>
+              <ListItemText
+                primary={controller.service.name}
+                secondary="Name"
+              />
+            </ListItem>
+
+            {controller.service.group !== undefined && (
+              <ListItem divider>
+                <ListItemIcon>
+                  <Group />
+                </ListItemIcon>
+                <ListItemText
+                  primary={controller.service.group}
+                  secondary="Group"
+                />
+              </ListItem>
+            )}
+
+            {controller.service.repository !== undefined && (
+              <ListItem divider>
+                <ListItemIcon>
+                  <Code />
+                </ListItemIcon>
+                <ListItemText
+                  primary={controller.service.repository}
+                  secondary="Repository"
+                />
+              </ListItem>
+            )}
+
+            {controller.service.taskBoard !== undefined && (
+              <ListItem divider>
+                <ListItemIcon>
+                  <TaskAlt />
+                </ListItemIcon>
+                <ListItemText
+                  primary={controller.service.taskBoard}
+                  secondary="Task Board"
+                />
+              </ListItem>
+            )}
+          </List>
+        </Box>
+      )}
     </React.Fragment>
   );
 };
