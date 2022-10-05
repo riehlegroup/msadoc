3fffa9d95dec83f5268357952f0a1f3cb045b9a3
Georg Schwarz <dev@georg-schwarz.com>
2022-10-05 09:05:10 +0200

    Show group details in similar style as service details

--- frontend/src/components/main-page/groups-tree-page/group-details/group-details.tsx
+++ frontend/src/components/main-page/groups-tree-page/group-details/group-details.tsx
@@ -1,3 +1,17 @@
+import {
+  CenterFocusStrongOutlined,
+  DatasetOutlined,
+  Group,
+} from '@mui/icons-material';
+import {
+  Badge,
+  Box,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 import { useMatch } from 'react-router-dom';
 
@@ -14,7 +28,72 @@ export const GroupDetails: React.FC = () => {
 
   return (
     <React.Fragment>
-      Group Details {JSON.stringify(controller.group)}
+      {controller.group && (
+        <Box
+          sx={{
+            overflowX: 'hidden',
+            overflowY: 'auto',
+            padding: 4,
+            maxWidth: '700px',
+          }}
+        >
+          <Typography variant="h3">Group Information</Typography>
+
+          {Object.keys(controller.group).includes('name') && (
+            <List>
+              <ListItem divider>
+                <ListItemIcon>
+                  <Badge />
+                </ListItemIcon>
+                <ListItemText
+                  primary={(controller.group as ServiceDocsGroup).name}
+                  secondary="Name"
+                />
+              </ListItem>
+            </List>
+          )}
+
+          {Object.keys(controller.group).includes('identifier') && (
+            <List>
+              <ListItem divider>
+                <ListItemIcon>
+                  <Group />
+                </ListItemIcon>
+                <ListItemText
+                  primary={(controller.group as ServiceDocsGroup).identifier}
+                  secondary="Full identifier"
+                />
+              </ListItem>
+            </List>
+          )}
+
+          <List>
+            <ListItem divider>
+              <ListItemIcon sx={{ color: 'inherit' }}>
+                <CenterFocusStrongOutlined />
+              </ListItemIcon>
+              <ListItemText
+                primary={`${controller.group.services.length} Services`}
+                secondary="Number of owned services"
+              />
+            </ListItem>
+          </List>
+
+          <List>
+            <ListItem divider>
+              <ListItemIcon sx={{ color: 'inherit' }}>
+                <DatasetOutlined />
+              </ListItemIcon>
+              <ListItemText
+                primary={`${
+                  Object.keys(controller.group.childGroups).length
+                } Groups`}
+                secondary="Number of owned groups"
+              />
+            </ListItem>
+          </List>
+        </Box>
+      )}
     </React.Fragment>
   );
 };
