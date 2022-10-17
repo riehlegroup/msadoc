8b5bf2d9956a8bc78aa44a5aad7c74a51d2440b0
Georg Schwarz <dev@georg-schwarz.com>
2022-10-17 11:01:33 +0200

    Activate API model validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -6,6 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('msdoc server API')
     .setDescription('The msdoc server API description')
