d084b485ecb58df59ed13c6018474c9fa46477ee
Georg Schwarz <dev@georg-schwarz.com>
2022-11-22 07:57:58 +0100

    Extract ServiceDocsHttpService

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './router';
 import { AuthDataServiceContextProvider } from './services/auth-data-service';
 import { HttpServiceContextProvider } from './services/http-service';
+import { ServiceDocsHttpServiceContextProvider } from './services/service-docs.http-service';
 
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
       <AuthDataServiceContextProvider>
         <HttpServiceContextProvider>
-          <AppRouter />
+          <ServiceDocsHttpServiceContextProvider>
+            <AppRouter />
+          </ServiceDocsHttpServiceContextProvider>
         </HttpServiceContextProvider>
       </AuthDataServiceContextProvider>
     </BrowserRouter>
--- frontend/src/services/http-service.tsx
+++ frontend/src/services/http-service.tsx
@@ -1,15 +1,11 @@
-import { AuthApi, Configuration, ServiceDocsApi } from 'msadoc-client';
+import { AuthApi, Configuration } from 'msadoc-client';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { firstValueFrom } from 'rxjs';
 import { AjaxConfig } from 'rxjs/ajax';
 
 import { ENVIRONMENT } from '../env';
-import {
-  ListAllServiceDocsHttpResponse,
-  LoginHttpResponse,
-  UnknownHttpError,
-} from '../models/api';
+import { LoginHttpResponse, UnknownHttpError } from '../models/api';
 import { APP_ROUTES } from '../routes';
 
 import {
@@ -27,9 +23,10 @@ interface HttpService {
     password: string,
   ) => Promise<LoginHttpResponse | UnknownHttpError>;
 
-  listAllServiceDocs: () => Promise<
-    ListAllServiceDocsHttpResponse | UnknownHttpError
-  >;
+  refreshAuthToken(): Promise<AccessAndRefreshToken | undefined>;
+
+  createConfiguration(accessToken?: string): Configuration;
+  getErrorStatus(error: unknown): number | undefined;
 }
 function useHttpService(): HttpService {
   const navigate = useNavigate();
@@ -149,74 +146,27 @@ function useHttpService(): HttpService {
   }
 
   /**
-   * @param refreshOn401 Should we try to refresh the Access Token if the server returns 401?
-   * @param accessToken The Access Token to use when performing the request. If no token is specified, the one provided by the AuthDataService is used.
+   * Get the "status" field of an error object returned when a http request fails.
    */
-  async function listAllServiceDocs(
-    refreshOn401: boolean,
-    accessToken?: string,
-  ): Promise<ListAllServiceDocsHttpResponse | UnknownHttpError> {
-    if (accessToken === undefined) {
-      accessToken = authDataService.state.accessAndRefreshToken?.accessToken;
-    }
-    if (accessToken === undefined) {
-      navigate(APP_ROUTES.login);
-      return {
-        status: 0,
-        data: undefined,
-      };
+  function getErrorStatus(error: unknown): number | undefined {
+    if (typeof error !== 'object' || error == null) {
+      return undefined;
     }
 
-    try {
-      const response = await firstValueFrom(
-        new ServiceDocsApi(
-          createConfiguration(accessToken),
-        ).serviceDocsControllerListAllServiceDocs(),
-      );
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
+    const status = (error as any).status;
 
-      return {
-        status: 200,
-        data: response,
-      };
-    } catch (error) {
-      if (!refreshOn401) {
-        return {
-          status: 0,
-          data: undefined,
-        };
-      }
-
-      const errorStatus = getErrorStatus(error);
-
-      if (errorStatus !== 401) {
-        return {
-          status: 0,
-          data: undefined,
-        };
-      }
-
-      // We might have an expired Access Token. --> Try refreshing it and then retry.
-
-      const refreshResult = await refreshAuthToken();
-      if (!refreshResult) {
-        return {
-          status: 0,
-          data: undefined,
-        };
-      }
-
-      return await listAllServiceDocs(false, refreshResult.accessToken);
+    if (typeof status !== 'number') {
+      return undefined;
     }
+    return status;
   }
 
   return {
     performLogin: performLogin,
-
-    listAllServiceDocs: (): Promise<
-      ListAllServiceDocsHttpResponse | UnknownHttpError
-    > => {
-      return listAllServiceDocs(true);
-    },
+    refreshAuthToken: refreshAuthToken,
+    createConfiguration: createConfiguration,
+    getErrorStatus: getErrorStatus,
   };
 }
 
@@ -247,20 +197,3 @@ export const useHttpServiceContext = (): HttpService => {
 
   return context;
 };
-
-/**
- * Get the "status" field of an error object returned when a http request fails.
- */
-function getErrorStatus(error: unknown): number | undefined {
-  if (typeof error !== 'object' || error == null) {
-    return undefined;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
-  const status = (error as any).status;
-
-  if (typeof status !== 'number') {
-    return undefined;
-  }
-  return status;
-}
