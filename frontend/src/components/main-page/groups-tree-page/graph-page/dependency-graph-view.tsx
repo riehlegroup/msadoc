52bc8167641d0d88460557c5966cdcb86dda781b
Georg Schwarz <dev@georg-schwarz.com>
2022-12-01 10:54:25 +0100

    Introduce custom styles

--- frontend/src/components/main-page/groups-tree-page/graph-page/dependency-graph-view.tsx
+++ frontend/src/components/main-page/groups-tree-page/graph-page/dependency-graph-view.tsx
@@ -1,4 +1,4 @@
-import { ElementDefinition } from 'cytoscape';
+import { ElementDefinition, Stylesheet } from 'cytoscape';
 import React from 'react';
 import CytoscapeComponent from 'react-cytoscapejs';
 
@@ -15,6 +15,36 @@ export const DependencyGraph: React.FC = () => {
   const controller = useController();
 
   const layout = { name: 'breadthfirst' };
+  const styleSheets: Stylesheet[] = [
+    {
+      selector: 'node',
+      style: {
+        color: 'black',
+        'background-color': 'data(backgroundColor)',
+        label: 'data(label)',
+        'font-size': 20,
+      },
+    },
+    {
+      selector: 'node:parent',
+      style: {
+        label: 'data(label)',
+        'text-valign': 'top',
+        'text-halign': 'center',
+        'text-max-width': '100px',
+        'text-margin-y': 30,
+        'font-weight': 'bold',
+        'padding-top': '50px',
+      },
+    },
+    {
+      selector: 'edge',
+      style: {
+        // Label: 'data(label)',
+        'line-color': 'data(lineColor)',
+      },
+    },
+  ];
 
   return (
     <React.Fragment>
@@ -22,6 +52,7 @@ export const DependencyGraph: React.FC = () => {
         elements={controller.state.cytoScapeElements}
         layout={layout}
         style={{ width: '100%', height: '100%' }}
+        stylesheet={styleSheets}
       />
       ;
     </React.Fragment>
@@ -49,7 +80,14 @@ function useController(): Controller {
 
   let elements: ElementDefinition[] = [];
   if (selectedGroup !== undefined) {
-    elements = new CyptoScapeBuilder().fromGroup(selectedGroup).build();
+    elements = new CyptoScapeBuilder({
+      serviceBackgroundColorFn: () => '#F71735',
+      groupBackgroundColorFn: getGroupColor,
+      apiEdgeColorFn: () => '#41EAD4',
+      eventEdgeColorFn: () => '#FF9F1C',
+    })
+      .fromGroup(selectedGroup)
+      .build();
   }
 
   const [state, setState] = React.useState<State>({
@@ -60,4 +98,16 @@ function useController(): Controller {
   return {
     state: state,
   };
+}
+
+function getGroupColor(group: RegularGroupNode): string {
+  let currentGroup: RegularGroupNode | RootGroupNode = group;
+  let distanceToRoot = 0;
+
+  while (currentGroup.type !== ServiceDocsTreeNodeType.RootGroup) {
+    currentGroup = currentGroup.parent;
+    ++distanceToRoot;
+  }
+  const hex = Number.parseInt(`${Math.min(6 + distanceToRoot, 16)}`, 16);
+  return '#' + `${hex}`.repeat(6);
 }
