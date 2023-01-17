7cce28f1f1254a4fe5301eeae18adeaecd85897e
MBuchalik <dev@martin-buchalik.de>
2023-01-17 14:55:06 +0100

    Switch to search-expression-parser

--- frontend/src/components/main-page/groups-tree-page/filter/models.ts
+++ frontend/src/components/main-page/groups-tree-page/filter/models.ts
@@ -1,46 +1,56 @@
-import { GetServiceDocResponse } from 'msadoc-client';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ExpressionNode, ExpressionNodeType } from 'search-expression-parser';
 
-export type FilterNode = AndNode | OrNode | NotNode | LiteralNode;
+/**
+ * Similar to {@link ExpressionNode}, but with some constraints and simplifications.
+ */
+export type FilterNode =
+  | FilterAndNode
+  | FilterOrNode
+  | FilterNotNode
+  | FilterKeyValueNode;
 
-export enum FilterNodeType {
-  Literal,
-  And,
-  Or,
-  Not,
-}
-
-export interface AndNode {
-  type: FilterNodeType.And;
-
-  leftChild: FilterNode;
-  rightChild: FilterNode;
-}
-export interface OrNode {
-  type: FilterNodeType.Or;
+export interface FilterAndNode {
+  type: ExpressionNodeType.And;
 
   leftChild: FilterNode;
   rightChild: FilterNode;
 }
-export interface NotNode {
-  type: FilterNodeType.Not;
+export interface FilterOrNode {
+  type: ExpressionNodeType.Or;
+
+  leftChild: FilterNode;
+  rightChild: FilterNode;
+}
+export interface FilterNotNode {
+  type: ExpressionNodeType.Not;
+
   child: FilterNode;
 }
-export interface LiteralNode {
-  type: FilterNodeType.Literal;
-  key: string;
+export interface FilterKeyValueNode {
+  type: ExpressionNodeType.KeyValue;
+
+  key: AllowedLiteralNodeKey;
   value: string;
 }
-
-type QueryKeyToServiceDocKeyMap = Record<string, keyof GetServiceDocResponse>;
 
 /**
  * In the filter queries, the allowed keys (slightly) differ from the keys that are present in the Service Docs.
  * For instance, we use "tag" instead of "tags" in the queries to better match the idea that we match a single tag.
  * This Map maps from a filter key to the respective Service Doc key.
  *
- * **IMPORTANT**: The keys must all be lowercase, because in our Semantics, we use ".toLowerCase()".
+ * **IMPORTANT**: The keys must all be lowercase, because we use ".toLowerCase()" when accessing entries in this Map.
  */
-export const QUERY_KEY_TO_SERVICEDOC_MAP: QueryKeyToServiceDocKeyMap = {
+/*
+  The following can be made more type-safe in TS 4.9 using the "satisfies" operator:
+  ```
+  type QueryKeyToServiceDocKeyMap = Record<string, keyof GetServiceDocResponse>;
+  export const QUERY_KEY_TO_SERVICEDOC_MAP = {...} satisfies QueryKeyToServiceDocKeyMap;
+  ```
+
+  Unfortunately, react-scripts 18.2.0 did not support this syntax yet, leading to a compiler error.
+*/
+export const QUERY_KEY_TO_SERVICEDOC_MAP = {
   name: 'name',
   tag: 'tags',
   group: 'group',
@@ -58,12 +68,26 @@ export const QUERY_KEY_TO_SERVICEDOC_MAP: QueryKeyToServiceDocKeyMap = {
 
   responsibleteam: 'responsibleTeam',
   responsible: 'responsibles',
-};
+} as const;
+
+type AllowedLiteralNodeKey = keyof typeof QUERY_KEY_TO_SERVICEDOC_MAP;
 
 /**
  * The strings that are allowed to be used as keys in a query.
  */
-export const allowedLiteralNodeKeys = Object.keys(QUERY_KEY_TO_SERVICEDOC_MAP);
+export const ALLOWED_LITERAL_NODE_KEYS = Object.keys(
+  QUERY_KEY_TO_SERVICEDOC_MAP,
+) as AllowedLiteralNodeKey[];
+
+export function isAllowedLiteralNodeKey(
+  key: string,
+): key is AllowedLiteralNodeKey {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
+  if (ALLOWED_LITERAL_NODE_KEYS.includes(key as any)) {
+    return true;
+  }
+  return false;
+}
 
 /**
  * In a query, one can write `%empty%` in order to match empty values.
@@ -73,3 +97,7 @@ export const allowedLiteralNodeKeys = Object.keys(QUERY_KEY_TO_SERVICEDOC_MAP);
  * - It is an empty array.
  */
 export const SPECIAL_EMPTY_TAG = '%empty%';
+
+export interface FilterParseError {
+  errorMessages: string[];
+}
